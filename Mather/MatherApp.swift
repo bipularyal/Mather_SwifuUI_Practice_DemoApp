f0226f855a3511691ba79f7a6abc3b0a725508b9
Bipul Aryal <59837845+bipularyal@users.noreply.github.com>
2024-10-23 13:29:35 -0500

    Initial Commit

---
+++ Mather/MatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatherApp.swift
+//  Mather
+//
+//  Created by Bipul Aryal on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatherTests/MatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  MatherTests.swift
+//  MatherTests
+//
+//  Created by Bipul Aryal on 10/23/24.
+//
+
+import Testing
+@testable import Mather
+
+struct MatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
